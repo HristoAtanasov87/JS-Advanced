9a2e7f0ed126dd45883ddba303cc7f7efa67d7e0
Hristo Atanasov <hristo_atanasov87@abv.bg>
2021-01-17 23:50:45 +0200

    new problem solved

---
+++ syntaxFunctionsAndStatements/wordsUppercase.js
@@ -0,0 +1,13 @@
+function solve(text) {
+    let pattern = /[A-Za-z]+/g;
+    let toPrint = [];
+
+    let result = text.match(pattern);
+    for (let el of result) {
+        toPrint.push(el.toUpperCase());
+    }
+
+    return toPrint.join(', ')
+}
+
+console.log(solve('Hi, how are you?'));
