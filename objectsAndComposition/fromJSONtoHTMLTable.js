ba061e1a299292f6fac4c8618bfcdd8ab598ecb6
Hristo Atanasov <hristo_atanasov87@abv.bg>
2021-01-22 15:01:10 +0200

    update

--- objectsAndComposition/fromJSONtoHTMLTable.js
+++ objectsAndComposition/fromJSONtoHTMLTable.js
@@ -1,4 +1,21 @@
 function solve(input) {
+    let symbolsToReplace = {
+        '&': '&amp;',
+        '<': '&lt;',
+        '>': '&gt;',
+        '\"': '&quot;',
+        '\'': '&#39;'
+    };
+
+    function escapeSymbols(text) {
+        return text
+            .split("&").join(symbolsToReplace["&"])
+            .split("<").join(symbolsToReplace["<"])
+            .split(">").join(symbolsToReplace[">"])
+            .split("\"").join(symbolsToReplace["\""])
+            .split("\'").join(symbolsToReplace["'"])
+    }
+
     let students = JSON.parse(input);
     let first = students[0];
 
@@ -7,13 +24,14 @@ function solve(input) {
     html += `\t<tr>${Object.keys(first).map(x => `<th>${x}</th>`).join('')}</tr>\n`;
     students.forEach(student => {
         html += '\t<tr>';
-        html += Object.values(student).map(x => `<td>${x}</td>`).join('');
+        html += Object.values(student).map(x => `<td>${isNaN(x) ? escapeSymbols(x) : x}</td>`).join('');
         html += '</tr>\n';
     });
     html += '</table>';
 
     return html;
 }
+
 
 
 console.log(solve('[{"Name":"Pesho","Score": 4,"Grade": 8},{"Name": "Gosho","Score": 5,"Grade": 8},{"Name": "Angel","Score": 5.50,"Grade": 10}]'
